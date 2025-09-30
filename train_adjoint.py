bd8752209e3b53d7617c3b32b81103b8a14e4bf2
lindliu <dliu2020@outlook.com>
2025-09-30 21:06:35 +0200

    'f'

--- train_adjoint.py
+++ train_adjoint.py
@@ -475,8 +475,8 @@ if __name__ == "__main__":
     # weighted = False # True # 
 
     
-    for data_name in ['burgers', 'fkpp']:
-    # for data_name in ['fkpp']:
+    # for data_name in ['burgers', 'fkpp']:
+    for data_name in ['fkpp']:
         if data_name=='fkpp':
             step = 1  ## 1, 2, 4, 5, 10, 20, 40
             num_samples = 2001//step ## 2000 ##
@@ -490,8 +490,8 @@ if __name__ == "__main__":
         ratio = str(split_ratio).replace('.','p')
         
         noise_level_list = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
-        for noise_level in noise_level_list:
-        # for noise_level in [0]:
+        # for noise_level in noise_level_list:
+        for noise_level in [80, 100, 120, 140, 160, 180, 200]:
             error_opinf_6_init_list, error_adjoint_init_list, error_opinf_2_init_list = [], [], []
             error_opinf_6_train_list, error_adjoint_train_list, error_opinf_2_train_list = [], [], []
             error_opinf_6_list, error_adjoint_list, error_opinf_2_list = [], [], []
