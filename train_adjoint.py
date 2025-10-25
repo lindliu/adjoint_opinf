6e9935da7e7f22efc50ed65ad1b77a86ae4f17a6
lindliu <dliu2020@outlook.com>
2025-10-25 19:59:11 +0200

    f

--- train_adjoint.py
+++ train_adjoint.py
@@ -513,14 +513,16 @@ if __name__ == "__main__":
         for data_name in ['burgers', 'fkpp']:
         # for data_name in ['fkpp']:
             if data_name=='fkpp':
-                step = 4 ## 1, 2, 4, 10
+                step = 10 ## 1, 2, 4, 10
                 num_samples = 2001//step ## 2000 ##
                 split_ratio = .75
                 
             if data_name=='burgers':
-                step = 100 # 1 # 10 # 100 # 500 # 
+                step = 500 # 1 # 10 # 100 # 500 # 
                 num_samples = 10000//step # 10000
                 split_ratio = .5
+            
+            assert split_ratio + split_ratio_validation < 1, 'percentage of train and validation data is more than 1!!'
             
             ratio = str(split_ratio).replace('.','p')
             
